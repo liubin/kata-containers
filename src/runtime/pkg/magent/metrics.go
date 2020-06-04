7a6275604b1a93854dae14bc864a439a62816776
bin liu <bin@hyper.sh>
2020-06-04 11:25:53 +0800

    remove duplicated metric HELP/TYPE directive of encoded prometheus output
    Signed-off-by: bin liu <bin@hyper.sh>

--- src/runtime/pkg/magent/metrics.go
+++ src/runtime/pkg/magent/metrics.go
@@ -81,99 +81,13 @@ func (ma *MAgent) getMetricsAddress(sandboxID, namespace string) (string, error)
 	return string(data), nil
 }
 
-// aggrateSandboxMetrics will get metrics from one sandbox and do some process
-func (ma *MAgent) aggrateSandboxMetrics(sandboxID, namespace string, w http.ResponseWriter, r *http.Request, encoder expfmt.Encoder) error {
-	socket, err := ma.getMetricsAddress(sandboxID, namespace)
-	if err != nil {
-		return err
-	}
-
-	transport := &http.Transport{
-		DisableKeepAlives: true,
-		Dial: func(proto, addr string) (conn net.Conn, err error) {
-			return net.Dial("unix", "\x00"+socket)
-		},
-	}
-
-	client := http.Client{
-		Timeout:   3 * time.Second,
-		Transport: transport,
-	}
-
-	resp, err := client.Get("http://shim/metrics")
-	if err != nil {
-		return err
-	}
-
-	defer func() {
-		resp.Body.Close()
-	}()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return err
-	}
-
-	reader := bytes.NewReader(body)
-	decoder := expfmt.NewDecoder(reader, expfmt.FmtText)
-
-	// decode metrics from sandbox to MetricFamily
-	list := make([]*dto.MetricFamily, 0)
-	for {
-		mf := &dto.MetricFamily{}
-		if err := decoder.Decode(mf); err != nil {
-			if err == io.EOF {
-				break
-			}
-			return err
-		}
-		list = append(list, mf)
-	}
-
-	// newList contains processed MetricFamily
-	newList := make([]*dto.MetricFamily, len(list))
-
-	for i := range list {
-		metricFamily := list[i]
-		metricList := metricFamily.Metric
-		for j := range metricList {
-			metric := metricList[j]
-			metric.Label = append(metric.Label, &dto.LabelPair{
-				Name:  mutils.String2Pointer("sandbox_id"),
-				Value: mutils.String2Pointer(sandboxID),
-			})
-		}
-
-		if metricFamily.Name != nil && (strings.HasPrefix(*metricFamily.Name, "go_") || strings.HasPrefix(*metricFamily.Name, "process_")) {
-			metricFamily.Name = mutils.String2Pointer("kata_shim_" + *metricFamily.Name)
-		}
-		newList[i] = metricFamily
-	}
-
-	// write encoded metrics to encoder
-	for _, mf := range newList {
-		if err := encoder.Encode(mf); err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
 // ProcessMetricsRequest get metrics from shim/hypervisor/vm/agent and return metrics to client.
 func (ma *MAgent) ProcessMetricsRequest(w http.ResponseWriter, r *http.Request) {
-	var (
-		sandboxes map[string]string
-		err       error
-	)
 	start := time.Now()
 
 	scrapeCount.Inc()
 	defer func() {
 		scrapeDurationsHistogram.Observe(float64(time.Since(start).Nanoseconds() / int64(time.Millisecond)))
-		if err != nil {
-			scrapeFailedCount.Inc()
-		}
 	}()
 
 	// gather metrics collected for management agent.
@@ -202,6 +116,13 @@ func (ma *MAgent) ProcessMetricsRequest(w http.ResponseWriter, r *http.Request) 
 	encoder := expfmt.NewEncoder(writer, contentType)
 
 	mfs, err := prometheus.DefaultGatherer.Gather()
+	if err != nil {
+		logrus.WithError(err).Error("failed to Gather metrics from prometheus.DefaultGatherer")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
 	for i := range mfs {
 		metricFamily := mfs[i]
 
@@ -215,15 +136,131 @@ func (ma *MAgent) ProcessMetricsRequest(w http.ResponseWriter, r *http.Request) 
 		}
 	}
 
+	// aggregate sandboxes metrics and write to response by encoder
+	if err := ma.aggregateSandboxMetrics(encoder); err != nil {
+		logrus.WithError(err).Errorf("failed aggregateSandboxMetrics")
+		scrapeFailedCount.Inc()
+	}
+}
+
+// aggregateSandboxMetrics will get metrics from one sandbox and do some process
+func (ma *MAgent) aggregateSandboxMetrics(encoder expfmt.Encoder) error {
 	// get all sandboxes from cache
-	sandboxes = ma.sandboxCache.getAllSandboxes()
+	sandboxes := ma.sandboxCache.getAllSandboxes()
 	// save running kata pods as a metrics.
 	runningShimCount.Set(float64(len(sandboxes)))
+
+	// sandboxMetricsList contains list of MetricFamily list from one sandbox.
+	sandboxMetricsList := make([][]*dto.MetricFamily, 0)
+
+	// get metrics from sandbox's shim
 	for sandboxID, namespace := range sandboxes {
-		// aggrate one sandbox's metrics
-		if e := ma.aggrateSandboxMetrics(sandboxID, namespace, w, r, encoder); e != nil {
-			logrus.Errorf("failed to aggrate one sandbox's metrics: %s", e.Error())
-			err = e
+		sandboxMetrics, err := ma.getSandboxMetrics(sandboxID, namespace)
+		if err != nil {
+			logrus.WithError(err).Errorf("failed to get metrics for sandbox: %s", sandboxID)
+			continue
+		}
+		sandboxMetricsList = append(sandboxMetricsList, sandboxMetrics)
+	}
+
+	// metricsMap used to aggregate metirc from multiple sandboxes
+	// key is MetricFamily.Name, and value is list of MetricFamily from multiple sandboxes
+	metricsMap := make(map[string]*dto.MetricFamily)
+	// merge MetricFamily list for the same MetricFamily.Name from multiple sandboxes.
+	for i := range sandboxMetricsList {
+		sandboxMetrics := sandboxMetricsList[i]
+		for j := range sandboxMetrics {
+			mf := sandboxMetrics[j]
+			key := *mf.Name
+
+			// add MetricFamily.Metric to the exists MetricFamily instance
+			if oldmf, found := metricsMap[key]; found {
+				oldmf.Metric = append(oldmf.Metric, mf.Metric...)
+			} else {
+				metricsMap[key] = mf
+			}
 		}
 	}
+
+	// write metrics to response.
+	for _, mf := range metricsMap {
+		if err := encoder.Encode(mf); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// getSandboxMetrics will get sandbox's metrics from shim
+func (ma *MAgent) getSandboxMetrics(sandboxID, namespace string) ([]*dto.MetricFamily, error) {
+	socket, err := ma.getMetricsAddress(sandboxID, namespace)
+	if err != nil {
+		return nil, err
+	}
+
+	transport := &http.Transport{
+		DisableKeepAlives: true,
+		Dial: func(proto, addr string) (conn net.Conn, err error) {
+			return net.Dial("unix", "\x00"+socket)
+		},
+	}
+
+	client := http.Client{
+		Timeout:   3 * time.Second,
+		Transport: transport,
+	}
+
+	resp, err := client.Get("http://shim/metrics")
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() {
+		resp.Body.Close()
+	}()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	reader := bytes.NewReader(body)
+	decoder := expfmt.NewDecoder(reader, expfmt.FmtText)
+
+	// decode metrics from sandbox to MetricFamily
+	list := make([]*dto.MetricFamily, 0)
+	for {
+		mf := &dto.MetricFamily{}
+		if err := decoder.Decode(mf); err != nil {
+			if err == io.EOF {
+				break
+			}
+			return nil, err
+		}
+		list = append(list, mf)
+	}
+
+	// newList contains processed MetricFamily
+	newList := make([]*dto.MetricFamily, len(list))
+
+	for i := range list {
+		metricFamily := list[i]
+		metricList := metricFamily.Metric
+		for j := range metricList {
+			metric := metricList[j]
+			metric.Label = append(metric.Label, &dto.LabelPair{
+				Name:  mutils.String2Pointer("sandbox_id"),
+				Value: mutils.String2Pointer(sandboxID),
+			})
+		}
+
+		// Kata shim are using prometheus go client, add an prefix for metric name to avoid confusing
+		if metricFamily.Name != nil && (strings.HasPrefix(*metricFamily.Name, "go_") || strings.HasPrefix(*metricFamily.Name, "process_")) {
+			metricFamily.Name = mutils.String2Pointer("kata_shim_" + *metricFamily.Name)
+		}
+		newList[i] = metricFamily
+	}
+
+	return newList, nil
 }
