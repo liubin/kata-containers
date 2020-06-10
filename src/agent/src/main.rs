1fad4aaf98c78e41b07a11936fe96e7662d7db2d
bin liu <bin@hyper.sh>
2020-06-10 16:13:31 +0800

    agent: add GetMetrics implementation
    add implementation of `GetMetrics` for agent.
    
    Signed-off-by: bin liu <bin@hyper.sh>

--- src/agent/src/main.rs
+++ src/agent/src/main.rs
@@ -12,6 +12,7 @@
 extern crate lazy_static;
 extern crate oci;
 extern crate prctl;
+extern crate prometheus;
 extern crate protocols;
 extern crate regex;
 extern crate rustjail;
@@ -49,6 +50,7 @@ use unistd::Pid;
 mod config;
 mod device;
 mod linux_abi;
+mod metrics;
 mod mount;
 mod namespace;
 mod network;
